52dd431597c9d5e65924e71d526bd57db728b688
AlbertLara <thepivot88@gmail.com>
2020-12-30 20:23:40 +0100

    Remove groups and roles and apply approval request

---
+++ run.py
@@ -0,0 +1,11 @@
+from app import create_app
+
+from decouple import config as config_decouple
+
+ENV = 'LOCALHOST'
+if config_decouple('PRODUCTION',default=False):
+    ENV = 'PROD'
+app = create_app(ENV)
+if __name__== '__main__':
+    if app is not None:
+        app.run(host='localhost',port=9000)
