8c1466550000623f33bc7ac780f774bec7f80528
AlbertLara <thepivot88@gmail.com>
2021-03-12 08:27:07 +0100

    .

--- app/project/utils/token.py
+++ app/project/utils/token.py
@@ -2,7 +2,6 @@ from flask import jsonify, request, abort
 from flask_login import current_user
 from functools import wraps, update_wrapper
 from itsdangerous import URLSafeTimedSerializer
-from ..project import app
 
 def admin_role_required(f):
     @wraps(f)
@@ -12,7 +11,7 @@ def admin_role_required(f):
         return f(*args,**kwargs)
     return decorator
 
-
+"""
 def generate_confirmation_token(email):
     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
     return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
@@ -24,5 +23,5 @@ def confirm_token(token, expiration=3600):
         email = serializer.loads(token,salt=app.config['SECURITY_PASSWORD_SALT'],max_age=expiration)
     except:
         return False
-    return email
+    return email"""
 
